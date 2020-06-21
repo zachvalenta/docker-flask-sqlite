a8466697aa53ce7fc278f7a7b36547e3ff7ca746
zachvalenta <zjv5002@gmail.com>
2020-06-21 09:47:44 -0400

    impl - add db
    * conf db
    * declare model
    * create tables on app start

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
+# construct path to SQLite file
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, os.getenv("SQLITE_FILE"))
+db_uri = "sqlite:///" + db_path
+
+# init and config app, init db
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# declare model
+class Thing(db.Model):
+    pk = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+    def __repr__(self):
+        return f"id {self.pk} name {self.name}"
 
 
-@app.route('/index')
+# create tables on app start
+db.create_all()
+
+
 @app.route('/healthcheck')
 def index():
-    return f"you set the env var FOO to: {os.getenv('FOO')}"
     return "app runnning", 200
